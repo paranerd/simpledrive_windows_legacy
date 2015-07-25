cdeb6ae4982eb9d0b2ded8fb1baa8b3cd722cc9f
paranerd <in.my.digital.world@gmail.com>
2015-07-25 12:54:15 +0200

    first release

---
+++ sd sync client/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace sd_sync_client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label3 = new System.Windows.Forms.Label();
+            this.server_input = new System.Windows.Forms.TextBox();
+            this.user_input = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pass_input = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.status = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label6 = new System.Windows.Forms.Label();
+            this.sync_folder = new System.Windows.Forms.Label();
+            this.browse = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Server:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // server_input
+            // 
+            this.server_input.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.server_input.Location = new System.Drawing.Point(115, 142);
+            this.server_input.Name = "server_input";
+            this.server_input.Size = new System.Drawing.Size(152, 26);
+            this.server_input.TabIndex = 1;
+            this.server_input.TextChanged += new System.EventHandler(this.server_input_TextChanged);
+            // 
+            // user_input
+            // 
+            this.user_input.Location = new System.Drawing.Point(115, 179);
+            this.user_input.Name = "user_input";
+            this.user_input.Size = new System.Drawing.Size(152, 26);
+            this.user_input.TabIndex = 3;
+            this.user_input.TextChanged += new System.EventHandler(this.user_input_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 185);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "User:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // pass_input
+            // 
+            this.pass_input.Location = new System.Drawing.Point(115, 218);
+            this.pass_input.Name = "pass_input";
+            this.pass_input.PasswordChar = '*';
+            this.pass_input.Size = new System.Drawing.Size(152, 26);
+            this.pass_input.TabIndex = 5;
+            this.pass_input.TextChanged += new System.EventHandler(this.pass_input_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 224);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Password:";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // button2
+            // 
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button2.Location = new System.Drawing.Point(115, 295);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 41);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Connect";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.ForeColor = System.Drawing.Color.Red;
+            this.status.Location = new System.Drawing.Point(12, 325);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(0, 20);
+            this.status.TabIndex = 7;
+            this.status.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.status.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(245, 98);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.SelectedPath = "C:\\Users\\paranerd\\Desktop";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(18, 261);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(92, 20);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Sync folder:";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // sync_folder
+            // 
+            this.sync_folder.AutoEllipsis = true;
+            this.sync_folder.Location = new System.Drawing.Point(116, 257);
+            this.sync_folder.MaximumSize = new System.Drawing.Size(100, 0);
+            this.sync_folder.Name = "sync_folder";
+            this.sync_folder.Size = new System.Drawing.Size(100, 20);
+            this.sync_folder.TabIndex = 10;
+            this.sync_folder.Text = "Test";
+            this.sync_folder.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // browse
+            // 
+            this.browse.Location = new System.Drawing.Point(237, 257);
+            this.browse.Name = "browse";
+            this.browse.Size = new System.Drawing.Size(30, 24);
+            this.browse.TabIndex = 11;
+            this.browse.Text = "...";
+            this.browse.UseVisualStyleBackColor = true;
+            this.browse.Click += new System.EventHandler(this.browse_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button2;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.ClientSize = new System.Drawing.Size(279, 347);
+            this.Controls.Add(this.browse);
+            this.Controls.Add(this.sync_folder);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pass_input);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.user_input);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.server_input);
+            this.Controls.Add(this.label3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "simpleDrive Sync Client";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox server_input;
+        private System.Windows.Forms.TextBox user_input;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox pass_input;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label status;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label sync_folder;
+        private System.Windows.Forms.Button browse;
+    }
+}
+
---
+++ sd sync client/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using simpledrive_client;
+using System.Configuration;
+
+namespace sd_sync_client
+{
+    public partial class Form1 : Form
+    {
+        static string service_name = "simpleDrive Sync Client";
+        static string config_path = Directory.GetParent(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\shared.config";
+        static ExeConfigurationFileMap fileMap = new ExeConfigurationFileMap();
+        static Configuration config;
+
+        static string userdir = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            fileMap.ExeConfigFilename = config_path;
+            config = ConfigurationManager.OpenMappedExeConfiguration(fileMap, ConfigurationUserLevel.None);
+            if (!canReadConfig()) {
+                status.Text = "Config not found";
+                blockText(true);
+                return;
+            }
+
+            // Preset values
+            server_input.Text = (config.AppSettings.Settings["server"] != null) ? config.AppSettings.Settings["server"].Value : "";
+            user_input.Text = (config.AppSettings.Settings["user"] != null) ? config.AppSettings.Settings["user"].Value : "";
+            pass_input.Text = (config.AppSettings.Settings["pass"] != null) ? config.AppSettings.Settings["pass"].Value : "";
+            sync_folder.Text = (config.AppSettings.Settings["folder"] != null) ? new FileInfo(config.AppSettings.Settings["folder"].Value).Name : "";
+
+            ServiceController sc = new ServiceController(service_name);
+            if(sc == null)
+            {
+                return;
+            }
+            if (sc.Status == ServiceControllerStatus.Running)
+            {
+                blockText(true);
+                status.ForeColor = Color.Green;
+                status.Text = "Sync service running...";
+                button2.Text = "Disconnect";
+            }
+            else if (sc.Status == ServiceControllerStatus.Stopped)
+            {
+                button2.Text = "Connect";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!canReadConfig())
+            {
+                status.Text = "Config not found";
+                blockText(true);
+                return;
+            }
+            status.Text = "";
+            status.ForeColor = Color.Red;
+
+            ServiceController sc = new ServiceController(service_name);
+            if (sc.Status == ServiceControllerStatus.Running)
+            {
+                sc.Stop();
+                status.Text = "Sync service stopped";
+                button2.Text = "Connect";
+                blockText(false);
+            }
+            else if (sc.Status == ServiceControllerStatus.Stopped)
+            {
+                if (server_input.Text == "" || user_input.Text == "" || pass_input.Text == "" || userdir == "")
+                {
+                    status.Text = "No blank fields!";
+                    return;
+                }
+
+                blockText(true);
+                status.Text = "Connecting...";
+                string login = simpledrive.login(server_input.Text, user_input.Text, pass_input.Text);
+                if (login == null)
+                {
+                    status.Text = "Connection error";
+                    blockText(false);
+                }
+                else if (login == "1")
+                {
+                    var settings = new Dictionary<string, string>
+                    {
+                        { "server", server_input.Text },
+                        { "user", user_input.Text },
+                        { "pass", pass_input.Text },
+                        { "folder", sync_folder.Text }
+                    };
+                    writeSettings(settings);
+                    sc.Start();
+                    try
+                    {
+                        sc.WaitForStatus(ServiceControllerStatus.Running, new TimeSpan(0, 3000, 0));
+                        status.ForeColor = Color.Green;
+                        status.Text = "Sync service running...";
+                        button2.Text = "Disconnect";
+                        simpledrive.create_fav_link(userdir);
+                    }
+                    catch (System.ServiceProcess.TimeoutException)
+                    {
+                        status.Text = "Could not start sync service";
+                        blockText(false);
+                    }
+                }
+                else
+                {
+                    status.Text = "Login failed";
+                    blockText(false);
+                }
+            }
+        }
+
+        private void browse_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                userdir = folderBrowserDialog1.SelectedPath;
+                sync_folder.Text = new FileInfo(userdir).Name;
+            }
+        }
+
+        private bool canReadConfig()
+        {
+            if (!File.Exists(config_path))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void writeSettings(Dictionary<string, string> settings)
+        {
+            foreach (KeyValuePair<string, string> entry in settings)
+            {
+                config.AppSettings.Settings.Remove(entry.Key);
+                config.AppSettings.Settings.Add(entry.Key, entry.Value);
+            }
+            config.Save(ConfigurationSaveMode.Minimal);
+        }
+
+        private void blockText(bool block)
+        {
+            server_input.ReadOnly = block;
+            user_input.ReadOnly = block;
+            pass_input.ReadOnly = block;
+            if (block)
+            {
+                server_input.BorderStyle = BorderStyle.None;
+                user_input.BorderStyle = BorderStyle.None;
+                pass_input.BorderStyle = BorderStyle.None;
+            }
+            else
+            {
+                server_input.BorderStyle = BorderStyle.FixedSingle;
+                user_input.BorderStyle = BorderStyle.FixedSingle;
+                pass_input.BorderStyle = BorderStyle.FixedSingle;
+            }
+        }
+
+        private void server_input_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void user_input_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pass_input_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
